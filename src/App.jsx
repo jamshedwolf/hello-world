4c540fcd5976300525ba85436c8340f4e8aa4240
jamshedwolf <jamsheedwolf.com>
2025-10-21 05:54:32 -0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="h-screen flex bg-black items-center justify-center ">
+
+         <h1 className='font-bold text-white text-4xl '>hello World i am learning react js with tailwind css</h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
